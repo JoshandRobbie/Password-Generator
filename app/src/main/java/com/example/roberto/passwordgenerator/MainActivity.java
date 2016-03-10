a826c5fb9a902364689c94ec5fd231629252a1a4
Joshua Cantero <cante008@cougars.csusm.edu>
2016-03-09 22:41:19 -0800

    Connected viewPager with tabLayout.
    A couple maintenance fixes as well.

--- app/src/main/java/com/example/roberto/passwordgenerator/MainActivity.java
+++ app/src/main/java/com/example/roberto/passwordgenerator/MainActivity.java
@@ -1,17 +1,13 @@
 package com.example.roberto.passwordgenerator;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    TabLayout tabLayout;
     Adapter adapter;
     ViewPager pager;
 
@@ -19,8 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        adapter = new Adapter(getSupportFragmentManager());
+        pager = (ViewPager) findViewById(R.id.vPager);
+        pager.setAdapter(adapter);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(pager);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pager.setCurrentItem(tab.getPosition());
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                pager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                pager.setCurrentItem(tab.getPosition());
+            }
+        });
 
 
     }
