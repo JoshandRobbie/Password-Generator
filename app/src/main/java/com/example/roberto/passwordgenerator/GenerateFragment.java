3a39271c8dae64613d94242a830216e32939d391
Joshua Cantero <cante008@cougars.csusm.edu>
2016-03-16 23:45:11 -0700

    Generate Password fix.

--- app/src/main/java/com/example/roberto/passwordgenerator/GenerateFragment.java
+++ app/src/main/java/com/example/roberto/passwordgenerator/GenerateFragment.java
@@ -1,9 +1,7 @@
 package com.example.roberto.passwordgenerator;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.util.*;
+import java.util.Random;
 
 /**
  * Created by Robert Ortiz
@@ -102,12 +100,8 @@ public class GenerateFragment extends Fragment {
 */
     public String generate(int x) {
 
-       // Scanner vitamin = new Scanner(System.in); //Scanner for randomString Function
+
         Random password = new Random(); // Object used to create Random String
-       // int c; // Used for user input on regarding the amount of characters he/she wishes to use in their password
-
-
-        //Array[52]
         //Array contains the alphabet so that the program can use the letters to create a password
         String[] alphabet = {"A", "a", "B", "b", "C", "c", "D", "d", "E", "e", "F", "f", "G", "g", "H", "h", "I", "i", "J", "j", "K", "k", "L", "l",
                 "M", "m", "N", "n", "O", "o", "P", "p", "Q", "q", "R", "r", "S", "s", "T", "t", "U", "u", "V", "v", "W", "w", "X", "x", "Y", "y", "Z", "z"};
@@ -118,8 +112,6 @@ public class GenerateFragment extends Fragment {
          *
          */
 
-
-       // int c = vitamin.nextInt();
         int c = x;
         int nc = (c / 2) + 1;
         int ncm = (nc / 2);
@@ -133,12 +125,11 @@ public class GenerateFragment extends Fragment {
 
             for (int i = 0; i < totalValues; i++) {
                 int alphaNum = password.nextInt(52);
-                passWord1 = passWord1 + alphaNum; //System.out.print(alphabet[alphaNum]);
+                passWord1 = passWord1 + alphabet[alphaNum];
                 int numNum = password.nextInt(10);// gives a range from 0-10
-                passWord1 = passWord1 + numNum; // System.out.print(numNum);
+                passWord1 = passWord1 + numNum;
             } // incorporates the number into the password
 
-            //System.out.println();
             return passWord1;
         } else
 
@@ -147,15 +138,11 @@ public class GenerateFragment extends Fragment {
             for (int i = 0; i < ncm + 1; i++) {
 
                 int alphaNum = password.nextInt(52);
-                passWord2 = passWord2 + alphaNum ; //System.out.print(alphabet[alphaNum]);
+                passWord2 = passWord2 + alphabet[alphaNum];
                 int numNum = password.nextInt(10);
-                passWord2 = passWord2 + numNum; // System.out.println(numNum);
-
-
+                passWord2 = passWord2 + numNum;
             }
             return passWord2;
-
-            //System.out.print(numNum +"\n");
 
         }
     }
